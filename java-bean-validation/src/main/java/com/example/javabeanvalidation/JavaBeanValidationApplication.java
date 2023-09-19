a0fbc5ea197745a2fe68a2d44167278b2a12a5ab
bekar guliashvili <beka@bekars-MBP-2.lan>
2023-09-19 19:17:00 -0400

    initial commit

---
+++ java-bean-validation/src/main/java/com/example/javabeanvalidation/JavaBeanValidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.javabeanvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeanValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBeanValidationApplication.class, args);
+    }
+
+}
---
+++ java-bean-validation/src/test/java/com/example/javabeanvalidation/JavaBeanValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javabeanvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeanValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
